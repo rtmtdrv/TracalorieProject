1734afbb02ac0ff1fc4588bf2b118663a5760524
Art Todorov <art.todorov@me.com>
2020-03-31 23:39:56 -0700

    controllers & data structures

--- app.js
+++ app.js
@@ -2,7 +2,29 @@
 
 // Item Controller
 const ItemCtrl = (function() {
-  
+  // Item Constructor
+  const Item = function(id, name, calories) {
+    this.id = id;
+    this.name = name;
+    this.calories = calories;
+  }
+
+  // Data Structures / State
+  const data = {
+    items: [
+      {id: 0, name: 'Steak Dinner', calories: 1200},
+      {id: 1, name: 'Cookie', calories: 400},
+      {id: 2, name: 'Eggs', calories: 300}
+    ],
+    currentItem: null,
+    totalCalories: 0
+  }
+
+  return {
+    logData: function(){
+      return data;
+    }
+  }
 })();
 
 // UI Controller
@@ -11,6 +33,16 @@ const UICtrl = (function() {
 })();
 
 // App Controller
-const App = (function() {
-  
-})();
+const App = (function(ItemCtrl, UICtrl) {
+
+  // Public methods
+  return {
+    init: function(){
+      console.log('Initializing App..');
+    }
+  }
+})(ItemCtrl, UICtrl);
+
+// Initializing App
+App.init();
+
